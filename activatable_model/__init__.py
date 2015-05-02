ba90fcf96861bfb6daf72b46778544be5bc53a1d
Wes Kendall <wes.kendall@ambition.com>
2015-05-02 15:09:51 +0000

    removed unneeded comment

--- activatable_model/__init__.py
+++ activatable_model/__init__.py
@@ -1,5 +1,5 @@
 # flake8: noqa
-#from .models import BaseActivatableModel, ActivatableManager, ActivatableQuerySet
+from .models import BaseActivatableModel, ActivatableManager, ActivatableQuerySet
 from .signals import model_activations_changed
 from .version import __version__
 
