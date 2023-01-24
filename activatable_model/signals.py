d2c869cae84d4e549ce9a53ec256f365d1643515
Wes Okes <somewes@gmail.com>
2023-01-24 13:33:35 -0500

    fix signals

--- activatable_model/signals.py
+++ activatable_model/signals.py
@@ -1,6 +1,8 @@
 from django.dispatch import Signal
 
 
-model_activations_changed = Signal(providing_args=['instance_ids', 'is_active'])
+# providing_args=['instance_ids', 'is_active']
+model_activations_changed = Signal()
 
-model_activations_updated = Signal(providing_args=['instance_ids', 'is_active'])
+# providing_args=['instance_ids', 'is_active']
+model_activations_updated = Signal()
